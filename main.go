dfb44f6479d34c42972ec019369172fa0eed46b6
shaghasemi <shaghasemi@gmail.com>
2026-07-08 15:18:30 +0330

    quera - 3 - slice action using pointer

--- main.go
+++ main.go
@@ -1,77 +1,25 @@
 package main
 
-import (
-	"math"
-	"strconv"
-)
+import "slices"
 
-type FilterFunc func(int) bool
-type MapperFunc func(int) int
-
-func IsSquare(x int) bool {
-	sqrt := math.Sqrt(float64(x))
-	if sqrt == math.Trunc(sqrt) {
-		return true
-	}
-	return false
+func AddElement(numbers *[]int, element int) {
+	*numbers = append(*numbers, element)
 }
 
-func IsPalindrome(x int) bool {
-
-	if x < 0 {
-		return false
+func FindMin(numbers *[]int) int {
+	if len(*numbers) == 0 {
+		return 0
 	}
-
-	var stringifiedNumber string
-	var lengthOfNumber int
-
-	stringifiedNumber = strconv.Itoa(x)
-	lengthOfNumber = len(stringifiedNumber)
-
-	for i := 0; i < lengthOfNumber/2; i++ {
-		if stringifiedNumber[i] != stringifiedNumber[lengthOfNumber-i-1] {
-			return false
-		}
-	}
-
-	return true
+	return slices.Min(*numbers)
 }
 
-func Abs(num int) int {
-	if num < 0 {
-		return -num
-	}
-	return num
+func ReverseSlice(numbers *[]int) {
+	slices.Reverse(*numbers)
 }
 
-func Cube(num int) int {
-	return num * num * num
-}
-
-func Filter(input []int, f FilterFunc) []int {
-	var result []int
-	// for i := 0; i < len(input); i++ {
-	// 	if f(input[i]) {
-	// 		result = append(result, input[i])
-	// 	}
-	// }
-
-	for _, value := range input {
-		if f(value) {
-			result = append(result, value)
-		}
+func SwapElements(numbers *[]int, i, j int) {
+	if i < 0 || j < 0 || i >= len(*numbers) || j >= len(*numbers) {
+		return
 	}
-
-	return result
-}
-
-func Map(input []int, m MapperFunc) []int {
-	// var result []int
-	result := make([]int, 0, len(input))
-
-	for _, value := range input {
-		result = append(result, m(value))
-	}
-
-	return result
+	(*numbers)[i], (*numbers)[j] = (*numbers)[j], (*numbers)[i]
 }
